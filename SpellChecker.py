df19b23f119640387dd7ef8558710c225dc7b74e
Rachana <rachanasoma99@gmail.com>
2018-07-01 13:30:37 +0530

    Update SpellChecker.py

--- SpellChecker.py
+++ SpellChecker.py
@@ -6,23 +6,22 @@ test_sen = []
 
 testing_scr = open("input.txt", "r")
 value=""
-
 test_sent = testing_scr.read().split('.')
 mod = test_sent[0].split("=");
 test_sent[0] = mod[1]
 for i in range(0,len(test_sent)):
     test_sent[i] = test_sent[i].replace("+"," ")
     value = value + test_sent[i]+"."
-#print(testing_dict)
 print("***************************************************")
 # Create your own sentence or use one from the dataset
+total = 0
+count = 0
 filestr = ""
 for arr in test_sent:
     text = arr
     text = text.lower()
     text = text_to_ints(text)
 
-    
 
     checkpoint = "./kp=0.75,nl=3,th=0.75.ckpt"
     tf.reset_default_graph()
@@ -41,27 +40,30 @@ for arr in test_sent:
 
     # Remove the padding from the generated sentence
     pad = vocab_to_int["<PAD>"] 
-
+    
     print('\nText')
     print('  Word Ids:    {}'.format([i for i in text]))
     print('  Input Words: {}'.format("".join([int_to_vocab[i] for i in text])))
 
     pred = ""
+    
 
     print('\nSummary')
     print('  Word Ids:       {}'.format([i for i in answer_logits if i != pad]))
     print('  Response Words: {}'.format("".join([int_to_vocab[i] for i in answer_logits if i != pad])))
     pred = "".join([int_to_vocab[i] for i in answer_logits if i != pad])
-    filestr = filestr+pred+".\n"
+
+    arr = pred.split(" ");
+    for i in arr:
+        filestr = filestr+i+" "
+    filestr = filestr[:-1]
+    print(filestr)
+    filestr = filestr+" ."
     
 file = open("mod_input.txt",'w+')
 file.write(value)
 file.close()
 
-file=open("output.txt",'w+');
+file=open("output.txt",'w+')
 file.write(filestr)
-file.close()    
-    
-    
-    
-    
+file.close()
